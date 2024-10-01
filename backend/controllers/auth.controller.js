f83737f8825430f033de89aaf202c85b6ef96b0b
YD1234564789 <a8111202003@gmail.com>
2024-10-02 04:27:54 +0800

    add login & logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js"
 
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password ,confirmPassword, gender } = req.body
-
+    // 資料驗證
     if(password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" })
   
     const user = await User.findOne({ username })
@@ -24,27 +25,57 @@ export const signup = async (req, res) => {
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic
     })
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res)
+      await newUser.save()
 
-    await newUser.save()
-
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      gender: newUser.gender,
-      profilePic: newUser.profilePic
-    })
-
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic
+      })
+    } else {
+      res.status(400).json({ error: "Invalid user data" })
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message)
-    res.status(500).json({ message: "Internal Server Error" })
+    res.status(500).json({ error: "Internal Server Error" })
   }
 }
 
-export const login = (req, res) => {
-  res.send("login route")
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username })
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" })
+    }
+
+    generateTokenAndSetCookie(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic
+    })
+
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
 }
 
 export const logout = (req, res) => {
-  res.send("logout route")
+  try {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({message: "Logged out successfully"})
+  } catch (error) {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ error: "Internal Server Error" })
+  }
 }

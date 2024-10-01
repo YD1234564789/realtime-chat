6131b6b793004889686a6aecc7d560cedf93a63e
YD1234564789 <a8111202003@gmail.com>
2024-10-01 18:23:16 +0800

    create userModels & signUp controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,43 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password ,confirmPassword, gender } = req.body
+
+    if(password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" })
+  
+    const user = await User.findOne({ username })
+    if(user) return res.status(400).json({ message: "User already exists" })
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+    })
+
+    await newUser.save()
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      gender: newUser.gender,
+      profilePic: newUser.profilePic
+    })
+
   } catch (error) {
-    
+    console.log("Error in signup controller", error.message)
+    res.status(500).json({ message: "Internal Server Error" })
   }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import exprss from "express"
 import dotenv from "dotenv"
 
-import connectDB from "./config/mongoose.js"
 import authRoutes from "./routes/auth.routes.js"
-const app = exprss()
+import connectDB from "./config/mongoose.js"
 
 dotenv.config()
+const app = exprss()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  res.send("homeroute")
-})
 
+app.use(exprss.json())
 app.use("/api/auth", authRoutes)
+
+
+
 
 app.listen(PORT, () => {
   connectDB()

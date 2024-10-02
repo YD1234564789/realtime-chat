0f0341bd87fb0a29fcac951d8bc89290836c1c25
YD1234564789 <a8111202003@gmail.com>
2024-10-03 03:40:36 +0800

    add getUserForSidebar controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import exprss from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectDB from "./config/mongoose.js"
 
 dotenv.config()
@@ -11,8 +14,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(cookieParser())
 app.use(exprss.json())
+
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 
 

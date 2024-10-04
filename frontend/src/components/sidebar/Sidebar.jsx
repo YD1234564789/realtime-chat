ec5f4029143eca3510036dc2402b1138bf393a20
YD1234564789 <a8111202003@gmail.com>
2024-10-04 17:15:59 +0800

    add UI design

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import Conversations from "./Conversations"
+import SearchInput from "./SearchInput"
+import Logout from "./Logout"
+const Sidebar = () => {
+  return (
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+      <SearchInput/>
+      <div className="divider px-3"></div>
+      <Conversations/>
+      <Logout/>
+    </div>
+  )
+}
+
+export default Sidebar

b5fa0c9caa4144698ded1c2c984461d61d89814e
YD1234564789 <a8111202003@gmail.com>
2024-10-11 05:00:31 +0800

    add logout & login Functionality

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -65,7 +65,10 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">Sign UP</button>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700"disabled={loading}
+            >
+              {loading ? <span className="loading loading-spinner"></span> : "Sign UP"}
+            </button>
           </div>
         </form>
       </div>

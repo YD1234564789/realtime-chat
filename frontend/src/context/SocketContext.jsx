8c53aa65f9d2368ed5b15e1e0ac12ceb3c56a88a
YD1234564789 <a8111202003@gmail.com>
2024-10-29 03:39:16 +0800

    Socket url updated again

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if(authUser) {
       const socket = io(
-        "http://https://realtime-chat-kviq.onrender.com",
+        "https://realtime-chat-kviq.onrender.com",
         {
           query: {
             userId: authUser._id,

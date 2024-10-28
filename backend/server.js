25912e39a23c703f45aa83f4af89957054423e5c
YD1234564789 <a8111202003@gmail.com>
2024-10-29 02:34:05 +0800

    modify package

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import exprss from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js"
 dotenv.config()
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve() // 取得當前路徑
+
 app.use(cookieParser())
 app.use(exprss.json())
 
@@ -19,7 +22,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-
+app.use(exprss.static(path.join(__dirname, "/frontend/dist")))
+// 處理未匹配的路徑
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
